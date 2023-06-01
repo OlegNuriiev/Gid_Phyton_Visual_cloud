849c6247dc5a63023a52d00be45c0b07037fa01f
OlegNuriiev <91348199+OlegNuriiev@users.noreply.github.com>
2023-06-01 18:17:33 +0300

    Update Sample_ATG.py

--- Sample_ATG.py
+++ Sample_ATG.py
@@ -18,20 +18,22 @@ class SamplePars:
     def get_html(url, params=None):
         session = requests.Session()
         headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/113.0.0.0 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                      '*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'}
         datas = {
             'email': 'zakupki@masterservice.ms',
             'password': 'master_service'
 
         }
-        session.post('https://atg-ua.com.ua/avtorizaciya', data=datas, headers=headers, params=None,
+        session.post('https://atg-ua.com.ua/login/', data=datas, headers=headers, params=None,
                      allow_redirects=True)
-        session.post('https://atg-ua.com.ua/lichniy-kabinet', data=datas, headers=headers, params=None,
+        session.post('https://atg-ua.com.ua/edit-account/', data=datas, headers=headers, params=None,
                      allow_redirects=True)
-        session.post('https://atg-ua.com.ua/avtorizaciya', data=datas, headers=headers, params=None,
+        session.post('https://atg-ua.com.ua/login/', data=datas, headers=headers, params=None,
                      allow_redirects=True)
-        session.post('https://atg-ua.com.ua/lichniy-kabinet', data=datas, headers=headers, params=None,
+        session.post('https://atg-ua.com.ua/edit-account/', data=datas, headers=headers, params=None,
                      allow_redirects=True)
         r = session.get(url, headers=headers, params=params)
 
@@ -39,7 +41,7 @@ class SamplePars:
 
     @staticmethod
     def save_file(items, path):
-        with open(path, 'w', newline='') as file:
+        with open(path, 'w', newline='', encoding='utf-8-sig') as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(['id', 'Article', 'Description', 'Availability', 'Manufacturer', 'link', 'uah_price'])
             for item in items:
@@ -71,46 +73,34 @@ class AtgParsAgregats(SamplePars):
 
     @staticmethod
     def get_content(html):
-        items = BeautifulSoup(html, 'html.parser').find_all('div', class_='product-block-inner')
+        items = BeautifulSoup(html, 'html.parser').find_all('div', class_='boxCategory_li')
         attribute = []
         for item in items:
-            availability = 'Yes' if (item.find('div', class_='availability-triangle')) else 'No'
+
+            id = str(item.find('button', {'onclick': True}).get('onclick')[len("open_cart_modal('"):-len("');")]) \
+                if item.find('button', {'onclick': True}).get('onclick') else "No"
+
+            availability = 'No' if item.find \
+                ('button', class_='sliderProduct_link disabled_button flex j-c_center a-i_center') else 'Yes'
+
+            uah_price = item.find('i', class_='currenySymbol').previous_sibling.get_text(strip=True).replace \
+                ('.', ',') if item.find('i', class_='currenySymbol') else item.find \
+                ('div', class_='sliderProduct_boxPrice').get_text(strip=True)
+
+            manufacturer = str(item.find('ul', class_='sliderProduct_description').find('span').next_sibling.get_text(
+                strip=True)) if item.find('ul', class_='sliderProduct_description').find('span') else item.find(
+                'ul', class_='sliderProduct_description').findNext().findNext().get_text(strip=True)
+
             attribute.append({
-                'id': str(item.find('button', {'onclick': True}).get('onclick')[len("wishlist.add('"):-len("');")]),
-                'title': item.find('div', class_='articlemodel').get_text(strip=True),
-                'Description': item.find('div', class_='caption').find('a', class_='').get_text(strip=True),
+                'id': id,
+                'title': item.find('p', class_='sliderProduct_article').get_text(strip=True),
+                'Description': item.find('div', class_='sliderProduct_left').find('a').get_text(strip=True),
                 'Availability': availability,
-                'Manufacturer': item.find('div', class_='manufacturer_product').get_text(strip=True),
-                'uah_price': item.find('p', class_='price').get_text(strip=True).replace('.', ',').replace(' грн*', ''),
-                'link': item.find('a', class_='').get('href'),
+                'Manufacturer': manufacturer,
+                'uah_price': uah_price,
+                'link': item.find('a', class_='sliderProduct_thumb img-position').get('href'),
             })
         return attribute
-
-
-class AtgParsSeals(AtgParsAgregats):
-    @staticmethod
-    def get_content(html):
-        items = BeautifulSoup(html, 'html.parser').find_all('div', class_='product-block-inner')
-        attribute = []
-        for item in items:
-            availability = 'Yes' if (item.find('div', class_='availability-triangle')) else 'No'
-            attribute.append({
-                'title': item.find('div', class_='articlemodel').get_text(strip=True),
-                'Availability': availability,
-                'Manufacturer': item.find('div', class_='manufacturer_product').get_text(strip=True),
-                'uah_price': item.find('p', class_='price').get_text(strip=True),
-                'link': item.find('a', class_='').get('href'),
-            })
-        return attribute
-
-    @staticmethod
-    def save_file(items, path):
-        with open(path, 'w', newline='') as file:
-            writer = csv.writer(file, delimiter=';')
-            writer.writerow(['Article', 'Availability', 'Manufacturer', 'uah_price', 'link'])
-            for item in items:
-                writer.writerow(
-                    [item['title'], item['Availability'], item['Manufacturer'], item['uah_price'], item['link']])
 
 
 class GurService(SamplePars):
@@ -149,11 +139,12 @@ class GurService(SamplePars):
         attribute = []
         for item in items:
             availability = 'No' if (
-                item.find('div', class_='status table-cell').find('span', class_='stock-red')) else 'Yes'
+                item.find('button', class_='sliderProduct_link disabled_button flex j-c_center a-i_center')) else 'Yes'
             title = item.find('span', class_='car-model').findNext().get_text(strip=True) if item.find('span',
                                                                                                        class_='car-model') else item.find(
                 'a', class_='product-name').find('b').get_text(strip=True)
             attribute.append({
+                'id': str(item.find('a', {'onclick': True}).get('onclick')),
                 'title': title,
                 'Availability': availability,
                 'uah_price': item.find('div', class_='current-price').get_text(strip=True),
@@ -163,8 +154,8 @@ class GurService(SamplePars):
 
     @staticmethod
     def save_file(items, path):
-        with open(path, 'w', newline='') as file:
-            writer = csv.writer(file, delimiter=';')
+        with open(path, 'w', newline='', encoding='utf-8-sig') as file:
+            writer = csv.writer(file, delimiter=',')
             writer.writerow(['Article', 'Availability', 'uah_price', 'link'])
             for item in items:
                 writer.writerow([item['title'], item['Availability'], item['uah_price'], item['link']])
@@ -242,7 +233,7 @@ class Аpp_kiev(SamplePars):
     @staticmethod
     def save_file(items, path):
         with open(path, 'w', newline='', encoding='utf-8-sig') as file:
-            writer = csv.writer(file, delimiter=';')
+            writer = csv.writer(file, delimiter=',')
             writer.writerow(['Article', 'Availability', 'Size', 'Manufacturer', 'uah_price', 'link'])
             for item in items:
                 writer.writerow(
@@ -251,140 +242,145 @@ class Аpp_kiev(SamplePars):
 
 
 def list_other_gur():
-    object1 = AtgParsAgregats('https://atg-ua.com.ua/nasosy/gur', "GUR_pumps", 3)
+    object1 = AtgParsAgregats('https://atg-ua.com.ua/nasosy/gur', "GUR_pumps", 6)
     object1.parser()
 
-    # object3 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/gidravlicheskie', "StGUR", 22)
-    # object3.parser()
-    #
-    # object4 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/mehanicheskie', "StMEH", 5)
-    # object4.parser()
+    object3 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/gidravlicheskie', "StGUR", 48)
+    object3.parser()
+
+    object4 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/mehanicheskie', "StMEH", 12)
+    object4.parser()
 
 
 def list_other_eps():
-    object1 = AtgParsAgregats('https://atg-ua.com.ua/nasosy/egur', "EPS_pumps", 3)
+    object1 = AtgParsAgregats('https://atg-ua.com.ua/nasosy/egur', "EPS_pumps", 5)
     object1.parser()
 
-    object2 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/elektricheskie', "StEPS", 7)
+    object2 = AtgParsAgregats('https://atg-ua.com.ua/rulevye-reyki/elektricheskie', "StEPS", 13)
     object2.parser()
+
+    object3 = AtgParsAgregats('https://atg-ua.com.ua/elektropidsilyuvachi', "ELB_EPS", 3)
+    object3.parser()
 
 
 def list_other_Seals():
-    object4 = AtgParsSeals('https://atg-ua.com.ua/komplektuyushchie/salniki', "Seals", 29)
+    object4 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/c44-salniki', "Seals", 70)
     object4.parser()
 
 
 def list_other_emmetec():
-    object1 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_491', "БАЧКИ_И_КРЫШКИ",
-                              1)
-    object1.parser()
+    # object1 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/c16-bachki-ta-krishki', "БАЧКИ_И_КРЫШКИ", 2)
+    # object1.parser()
+    #
+    # object2 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/c33-vali', "ВАЛЫ", 10)
+    # object2.parser()
+    #
+    # object3 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/c15-datchiki-sensori-servotroniki',
+    #                           "ДАТЧИКИ, СЕНСОРЫ, СЕРВО", 3)
+    # object3.parser()
+    #
+    # object4 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/zaglushki', "ЗАГЛУШКИ", 1)
+    # object4.parser()
+    #
+    # object5 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_495', "ЗАЩИТНЫЕ КРЫШКИ",
+    #                           2)
+    # object5.parser()
+    #
+    # object6 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/kolpachki', "КОЛПАЧКИ, ЗАЩИТНЫЕ МАНЖЕТЫ", 1)
+    # object6.parser()
+    #
+    # object7 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/kolca-rezino-metal', "КОЛЬЦА РЕЗИНО-МЕТАЛ", 1)
+    # object7.parser()
+    #
+    # object8 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_497',
+    #                           "КОНТАКТНАЯ ГРУППА, РАЗЬЕМЫ", 3)
+    # object8.parser()
+    #
+    # object9 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_496', "КОРПУСА", 3)
+    # object9.parser()
+    #
+    # object10 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/krestoviny', "КРЕСТОВИНЫ", 1)
+    # object10.parser()
+    #
+    # object11 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_498',
+    #                            "МОТОРЫ, РЕДУКТОРА", 3)
+    # object11.parser()
+    #
+    # object12 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/podshipniki', "ПОДШИПНИКИ", 2)
+    # object12.parser()
+    #
+    # object13 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/porshni', "ПОРШНИ", 1)
+    # object13.parser()
+    #
+    # object14 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_494', "ПРОВОДА", 1)
+    # object14.parser()
+    #
+    # object15 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/pylniki-tyag', "ПЫЛЬНИКИ ТЯГ", 2)
+    # object15.parser()
+    #
+    # object16 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/raznoe', "РАЗНОЕ", 1)
+    # object16.parser()
+    #
+    # object17 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/raspredelitel', "РАСПРЕДЕЛИТЕЛЬ", 1)
+    # object17.parser()
+    #
+    # object18 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/regulirovochnye-vtulki',
+    #                            "РЕГУЛИРОВОЧНЫЕ ВТУЛКИ", 2)
+    # object18.parser()
+    #
+    # object19 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/rezinovye-o-ring', "РЕЗИНОВЫЕ O-RING", 8)
+    # object19.parser()
+    #
+    # object20 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_493', "РЕМНИ", 1)
+    # object20.parser()
+    #
+    # object21 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/saylentbloki', "САЙЛЕНТБЛОКИ", 1)
+    # object21.parser()
+    #
+    # object22 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/teflonovye-kolca', "ТЕФЛОНОВЫЕ КОЛЬЦА", 4)
+    # object22.parser()
+    #
+    # object23 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_492', "ТОРСИОНЫ", 2)
+    # object23.parser()
+    #
+    # object24 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/trubki', "ТРУБКИ", 2)
+    # object24.parser()
+    #
+    # object25 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/tyagi', "ТЯГИ", 1)
+    # object25.parser()
+    #
+    # object26 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/uplotniteli-rezino-metall',
+    #                            "УПЛОТНИТЕЛИ РЕЗИНО-МЕТАЛЛ", 2)
+    # object26.parser()
+    #
+    # object27 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/fole-vstavka', "ФОЛЬЕ ВСТАВКА", 2)
+    # object27.parser()
+    #
+    # object28 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/homuty', "ХОМУТЫ", 1)
+    # object28.parser()
+    #
+    # object29 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/shayby', "ШАЙБЫ", 1)
+    # object29.parser()
+    #
+    # object30 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/elektro-zapchasti', "ЭЛЕКТРО ЗАПЧАСТИ", 1)
+    # object30.parser()
 
-    object2 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/valy', "ВАЛЫ", 3)
-    object2.parser()
-
-    object3 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_499',
-                              "ДАТЧИКИ, СЕНСОРЫ, СЕРВО", 1)
-    object3.parser()
-
-    object4 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/zaglushki', "ЗАГЛУШКИ", 1)
-    object4.parser()
-
-    object5 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_495', "ЗАЩИТНЫЕ КРЫШКИ",
-                              2)
-    object5.parser()
-
-    object6 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/kolpachki', "КОЛПАЧКИ, ЗАЩИТНЫЕ МАНЖЕТЫ", 1)
-    object6.parser()
-
-    object7 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/kolca-rezino-metal', "КОЛЬЦА РЕЗИНО-МЕТАЛ", 1)
-    object7.parser()
-
-    object8 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_497',
-                              "КОНТАКТНАЯ ГРУППА, РАЗЬЕМЫ", 3)
-    object8.parser()
-
-    object9 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_496', "КОРПУСА", 3)
-    object9.parser()
-
-    object10 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/krestoviny', "КРЕСТОВИНЫ", 1)
-    object10.parser()
-
-    object11 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_498',
-                               "МОТОРЫ, РЕДУКТОРА", 3)
-    object11.parser()
-
-    object12 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/podshipniki', "ПОДШИПНИКИ", 2)
-    object12.parser()
-
-    object13 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/porshni', "ПОРШНИ", 1)
-    object13.parser()
-
-    object14 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_494', "ПРОВОДА", 1)
-    object14.parser()
-
-    object15 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/pylniki-tyag', "ПЫЛЬНИКИ ТЯГ", 2)
-    object15.parser()
-
-    object16 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/raznoe', "РАЗНОЕ", 1)
-    object16.parser()
-
-    object17 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/raspredelitel', "РАСПРЕДЕЛИТЕЛЬ", 1)
-    object17.parser()
-
-    object18 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/regulirovochnye-vtulki',
-                               "РЕГУЛИРОВОЧНЫЕ ВТУЛКИ", 2)
-    object18.parser()
-
-    object19 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/rezinovye-o-ring', "РЕЗИНОВЫЕ O-RING", 8)
-    object19.parser()
-
-    object20 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_493', "РЕМНИ", 1)
-    object20.parser()
-
-    object21 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/saylentbloki', "САЙЛЕНТБЛОКИ", 1)
-    object21.parser()
-
-    object22 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/teflonovye-kolca', "ТЕФЛОНОВЫЕ КОЛЬЦА", 4)
-    object22.parser()
-
-    object23 = AtgParsAgregats('https://atg-ua.com.ua/index.php?route=product/category&path=438_492', "ТОРСИОНЫ", 2)
-    object23.parser()
-
-    object24 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/trubki', "ТРУБКИ", 2)
-    object24.parser()
-
-    object25 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/tyagi', "ТЯГИ", 1)
-    object25.parser()
-
-    object26 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/uplotniteli-rezino-metall',
-                               "УПЛОТНИТЕЛИ РЕЗИНО-МЕТАЛЛ", 2)
-    object26.parser()
-
-    object27 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/fole-vstavka', "ФОЛЬЕ ВСТАВКА", 2)
-    object27.parser()
-
-    object28 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/homuty', "ХОМУТЫ", 1)
-    object28.parser()
-
-    object29 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/shayby', "ШАЙБЫ", 1)
-    object29.parser()
-
-    object30 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/elektro-zapchasti', "ЭЛЕКТРО ЗАПЧАСТИ", 1)
-    object30.parser()
+    object31 = AtgParsAgregats('https://atg-ua.com.ua/komplektuyushchie/elektro-zapchasti', "КОМПЛЕКТУЮЧІ", 283)
+    object31.parser()
 
 
 def list_other_nasosy_reyki():
     object6 = GurService('https://nasosy-reyki.com.ua/tovary/',
-                         "GURS_other", 325)
+                         "GURS_other", 322)
     object6.parser()
 
-    # object6 = GurService('https://nasosy-reyki.com.ua/ru/tovary/rulevaya-reika/',
-    #                      "GURS_reyki", 216)
-    # object6.parser()
-    #
-    # object6 = GurService('https://nasosy-reyki.com.ua/ru/tovary/nasosy/',
-    #                      "GURS_nasosy", 101)
-    # object6.parser()
+    object6 = GurService('https://nasosy-reyki.com.ua/ru/tovary/rulevaya-reika/',
+                         "GURS_reyki", 216)
+    object6.parser()
+
+    object6 = GurService('https://nasosy-reyki.com.ua/ru/tovary/nasosy/',
+                         "GURS_nasosy", 101)
+    object6.parser()
 
 
 def list_other_Аpp_kiev():
