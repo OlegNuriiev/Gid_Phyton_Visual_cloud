ffbfc25a3c49bef76177a2a024fcc818e4aeef3f
OlegNuriiev <91348199+OlegNuriiev@users.noreply.github.com>
2023-12-14 08:31:23 +0200

    Update engineparts1.py

--- engineparts1.py
+++ engineparts1.py
@@ -57,19 +57,25 @@ def pars_car_links():
     for link_pars in link_cars_pars:
         link = link_pars
         c = requests.get(url=link_pars, headers=headers)
+        soup3 = BeautifulSoup(c.text, 'html.parser')
         marks = BeautifulSoup(c.text, 'html.parser').find_all("span", itemprop="title")
-        title = BeautifulSoup(c.text, 'html.parser').find("h3", id="orderItemName").text
-        uah_price = BeautifulSoup(c.text, 'html.parser').find("strong", id="itemPrice").text
-        manufacturer = BeautifulSoup(c.text, 'html.parser').find("dd", style="width:65%").text \
-            if BeautifulSoup(c.text, 'html.parser').find("dd", style="width:65%").text else "no"
+        title = soup3.find("h3", id="orderItemName").text
+        uah_price = soup3.find("strong", id="itemPrice").text
+        manufacturer = soup3.find("dd", style="width:65%").text \
+            if soup3.find("dd", style="width:65%").text else "no"
 
-        applicability = BeautifulSoup(c.text, 'html.parser').find("p", style="color:#000;padding: 10px 1px;").text[
-                        len("Применимость:"):] if BeautifulSoup(c.text, 'html.parser').\
+        applicability = soup3.find("p", style="color:#000;padding: 10px 1px;").text[
+                        len("Применимость:"):] if soup3.\
             find("p", style="color:#000;padding: 10px 1px;").text else "no"
 
         description = BeautifulSoup(c.text, 'html.parser').find("div", id="prod-tab-1").text[
                       :-len("шаблоны для dle 11.2 ")].strip() if BeautifulSoup(c.text, 'html.parser').\
             find("div", id="prod-tab-1").text else "No"
+
+        soup3_engine_characteristics = soup3.find("dl", style="margin-top: -15px;").find_all("dd")
+
+        img_back = BeautifulSoup(c.text, 'html.parser').find("div", class_="flex-viewport").find("img")
+        print(img_back)
 
         attribute.append({
             "mark": marks[2].text,
@@ -78,7 +84,13 @@ def pars_car_links():
             'link': link,
             'Manufacturer': manufacturer,
             'Applicability': applicability,
-            'Description': description,
+            'Description': soup3_engine_characteristics[0].text,
+            'Volume': soup3_engine_characteristics[1].text,
+            'Pover': soup3_engine_characteristics[2].text,
+            'Mileage': soup3_engine_characteristics[3].text,
+            'Type': soup3_engine_characteristics[4].text,
+            'IMG': img_back,
+
         })
     return attribute
 
